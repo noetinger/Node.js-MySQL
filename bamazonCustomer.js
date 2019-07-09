f65984cce541369fb9e5947617a846bca158adda
Nick O <nicholas.oetinger@gmail.com>
2019-07-08 20:41:37 -0400

    Started inquirer functions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,5 @@
 const mysql = require("mysql");
-//const inquirer = require("inquirer");
+const inquirer = require("inquirer");
 
 //Create connection to sql database for data
 const connection = mysql.createConnection({
@@ -11,19 +11,55 @@ const connection = mysql.createConnection({
 });
 
 //Connect to the mysql server and sql database
-connection.connect(function (err){
-    if(err) throw err;
+connection.connect(function (err) {
+    if (err) throw err;
+    //run Start function
     start();
 })
 
-function start(){
+//Start function
+function start() {
     console.log("initiate start function")
-    connection.query("SELECT * FROM products", function(err, results){
-        let itemDisplay = [];
+    //run Display function
+    displayItems();
+};
+
+function displayItems() {
+    let itemDisplay = [];
+    //connect to database
+    connection.query("SELECT * FROM products", function (err, results) {
         if (err) throw err;
-        for (var i=0 ; i < results.length; i++){
+        //display each of the items in stock
+        for (var i = 0; i < results.length; i++) {
             itemDisplay.push(results[i]);
         }
         console.log(itemDisplay);
+        //run askItem function
+        AskItem();
     })
+};
+
+//Ask what item ID the customer wants to buy
+function AskItem() {
+    inquirer
+        .prompt([{
+            name: "id",
+            type: "input",
+            message: "Enter item ID for the item you'd like to buy: ",
+            // validate: function (value) {
+            //     //If the input does not equal a 
+            //     if (value !== itemDisplay[i].id) {
+            //         console.log("Please enter a valid ID");
+            //         AskItem();
+            //     };
+            // }
+        }])
+        .then(function (answer) {
+            console.log("AskItem function complete!")
+        })
+};
+
+//Ask how many of the item the customer wants to buy
+function howMany() {
+    
 };
