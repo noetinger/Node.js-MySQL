25d6a250dc9db4d8ea5481dc1003bb3fd7a0eab3
Nick O <nicholas.oetinger@gmail.com>
2019-07-09 18:29:23 -0400

    Added skeleton for functions and case-switch

--- bamazonManager.js
+++ bamazonManager.js
@@ -20,18 +20,57 @@ connection.connect(function (err) {
 })
 
 //Display tasks function using inquirer
-    // Case Switch If ____ task if selected, run ____ function
+function displayTasks() {
+    inquirer
+        .prompt([{
+            name: "action",
+            type: "list",
+            message: "Choose an option below to manage inventory:",
+            choices: ["View Products", "View Low Inventory", "Restock Inventory", "Add New Product"]
+        }])
+        // Case Switch If ____ task if selected, run ____ function
+        .then(function (answer) {
+            switch (answer.action) {
+                case 'View Products':
+                    viewProducts();
+                    break;
+                case 'View Low Inventory':
+                    viewLowInventory();
+                    break;
+                case 'Restock Inventory':
+                    restockInventory();
+                    break;
+                case 'Add New Product':
+                    addNewProduct();
+                    break;
+            }
+
+        })
+
+}
 
 //View Products for Sale
+function viewProducts(){
+
+}
     //List items
 
 //View Low Inventory
+function viewLowInventory(){
+
+}
     //List items with inventory amount less than 5
 
 //Add to Inventory
+function restockInventory(){
+
+}
     //prompt to add more to any item already in stock
     //Call function that adds item to that stock
 
 //Add new Product
+function addNewProduct(){
+
+}
     //prompt for info on new item to add.
     //call function that adds that item.
